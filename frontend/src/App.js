f3363c08d3f3959a76cdd1e0e7d15145d7a54f8d
Ramon Carceles <juanramoncarceles@gmail.com>
2020-12-02 20:27:21 +0100

    Changed HomeScreen component file name.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreens';
+import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
