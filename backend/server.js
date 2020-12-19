bec109ad1d4534ebd043d7580df9f58d28af7ff5
Ramon Carceles <juanramoncarceles@gmail.com>
2020-12-19 17:31:50 +0100

    Added custom error handler with express middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -15,6 +16,12 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+// Fallback for not found route errors.
+app.use(notFound);
+
+// Middleware error handler.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
